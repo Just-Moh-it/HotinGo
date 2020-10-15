eff35d3b1f080efbfeb65423952aeffa9a94ec6e
Mohit <Mohit@Mohits-MacBook-Air.local>
2020-10-15 17:36:13 +0530

    just testin

--- Desktop/HMS/Python/HMS/main.py
+++ Desktop/HMS/Python/HMS/main.py
@@ -5,7 +5,8 @@ import mysql.connector
 
 connection=mysql.connector.connect(host="remotemysql.com", user="OupAGhC9dM", password="KYVO7iezPw", database = "OupAGhC9dM", port="3306", charset="utf8", autocommit=True)
 cursor=connection.cursor()
-cursor.execute("insert into login (name, username, password) values ('Anirudh Agarwal', 'anirudh2004','Anirudh123');")
+
+
 cursor.execute("select * from login;")
 print(cursor.fetchall())
 
