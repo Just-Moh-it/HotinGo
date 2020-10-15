edabb03e1b5bc8c8f107067fe693d972342ba944
Mohit <Mohit@Mohits-MacBook-Air.local>
2020-10-15 17:33:57 +0530

    Added sql connectivity

--- Desktop/HMS/Python/HMS/main.py
+++ Desktop/HMS/Python/HMS/main.py
@@ -1,1 +1,14 @@
-#hello
+import tkinter as tk
+import mysql.connector
+
+# SQL Connectivity
+
+connection=mysql.connector.connect(host="remotemysql.com", user="OupAGhC9dM", password="KYVO7iezPw", database = "OupAGhC9dM", port="3306", charset="utf8", autocommit=True)
+cursor=connection.cursor()
+cursor.execute("insert into login (name, username, password) values ('Anirudh Agarwal', 'anirudh2004','Anirudh123');")
+cursor.execute("select * from login;")
+print(cursor.fetchall())
+
+
+# Tkinter
+
