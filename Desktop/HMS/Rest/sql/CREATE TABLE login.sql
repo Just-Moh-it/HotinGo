fecd5a0bb7ded879f6910e257e753a5151f541ce
Mohit <Mohit@Mohits-MacBook-Air.local>
2020-10-15 15:56:41 +0530

    first commit

---
+++ Desktop/HMS/Rest/sql/CREATE TABLE login.sql
@@ -0,0 +1,27 @@
+CREATE TABLE login
+(name varchar(30),
+username varchar(15) primary key,
+password varchar(10));
+
+CREATE TABLE reservations
+(r_id char(6) primary key,
+g_id char(4),
+r_date datetime,
+check_in datetime,
+check_out datetime,
+meal boolean,
+room_id char(4),
+r_type char(2));
+
+CREATE TABLE  guests
+(g_id char(4) Primary key,
+name varchar(30),
+address varchar(50),
+email_id varchar(50),
+phone bigint);
+
+CREATE TABLE rooms
+(room_id char(4) Primary key,
+room_no Int
+unique,price int,
+room_type char(2));
