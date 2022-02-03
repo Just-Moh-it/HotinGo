84d85aa5946e8500302c9ce6b7481e6935cf5733
Mohit Yadav <www.mohit2004@gmail.com>
2022-02-03 19:19:23 +0530

    🙉 Updated app to start with login screen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
-from new_gui.login.gui import loginWindow
-from new_gui.main_window.main import mainWindow
+from gui.login.gui import loginWindow
+from gui.main_window.main import mainWindow
 
 # Main window constructor
 root = tk.Tk()  # Make temporary window for app to start
@@ -9,7 +9,7 @@ root.withdraw()  # WithDraw the window
 
 if __name__ == "__main__":
 
-    # loginWindow()
-    mainWindow()
+    loginWindow()
+    # mainWindow()
 
     root.mainloop()
